9892b93f7f1a41f47f75afa4468843e898904500
Verduzco Martinez Franvcisco Manuel 230 <126191843+FrnciscoManuel123@users.noreply.github.com>
2023-05-24 12:05:42 -0600

    Add files via upload

---
+++ diferentes siclos.java
@@ -0,0 +1,45 @@
+siclo for
+
+import.java.io.*;
+public class drecremento
+  {
+   public static void main(String[]args)
+     {
+      for(int m=13;m>10;m--)
+      System.out.println("programacion 230");
+     }
+  } 
+
+siclo whiel
+
+import.java.io.*;
+public class drecremento
+  {
+   public static void main(String[]args)
+     {
+      int m=13;
+      while(m>10;m--)
+        {
+         System.out.println("programacion 230");
+        }  
+     }
+  }
+
+siclo do
+
+import.java.io.*;
+public class drecremento
+  {
+   public static void main(String[]args)
+     {
+      int m=13;
+      do(m>10;m--);
+        {
+         System.out.println("programacion 230");
+        }  
+     }
+  } 
+
+
+
+	 
